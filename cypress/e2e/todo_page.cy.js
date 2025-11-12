c70c813eec84a61b2425868a3b59e192bc74e972
Alejandro Fernández <afernandez@ecope.es>
2025-11-12 16:43:47 +0100

    test: Add Cypress tests for TodoMVC functionality including adding, completing, and incompleting todos

---
+++ cypress/e2e/todo_page.cy.js
@@ -0,0 +1,35 @@
+describe('todomvc.com page test', () => {
+
+  it('add todo', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Buy milk').type('{enter}')
+    cy.get('.todo-list li').should('have.length', 1)
+  })
+
+  it('mark todo as completed', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Buy bread').type('{enter}')
+    cy.get('[data-testid="todo-item-toggle"]').click()
+  })
+
+  it('mark todo as incompleted', () => {
+    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
+    cy.get('[data-testid="todo-item-toggle"]').click()
+    cy.get('[data-testid="todo-item-toggle"]').click()
+  })
+
+  // it('edit todo', () => {
+  //   cy.visit('https://todomvc.com/examples/react/dist/')
+  //   cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
+  //   cy.get('[data-testid="todo-item-label"]').dblclick()
+  //   cy.get('[data-testid="input-container"]').type('Buy beer').type('{enter}')
+  // })
+
+  // it('delete todo', () => {
+  //   cy.visit('https://todomvc.com/examples/react/dist/')
+  //   cy.get('[data-testid="text-input"]').type('Buy oranges').type('{enter}')
+  //   cy.get('[data-testid="todo-item-label"]').trigger('mouseover')
+  // })
+
+})
