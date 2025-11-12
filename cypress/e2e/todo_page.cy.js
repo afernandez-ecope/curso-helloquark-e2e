44d4e8fab9157a8909e7782dd50c5bf91080f46a
Alejandro Fernández <afernandez@ecope.es>
2025-11-12 16:49:50 +0100

    test: Enhance Cypress tests for TodoMVC by verifying todo item presence after addition, completion, and incompletion

--- cypress/e2e/todo_page.cy.js
+++ cypress/e2e/todo_page.cy.js
@@ -2,21 +2,32 @@ describe('todomvc.com page test', () => {
 
   it('add todo', () => {
     cy.visit('https://todomvc.com/examples/react/dist/')
+
     cy.get('[data-testid="text-input"]').type('Buy milk').type('{enter}')
+
     cy.get('.todo-list li').should('have.length', 1)
+    cy.get('.todo-list li').should('contain', 'Buy milk')
   })
 
   it('mark todo as completed', () => {
     cy.visit('https://todomvc.com/examples/react/dist/')
+
     cy.get('[data-testid="text-input"]').type('Buy bread').type('{enter}')
     cy.get('[data-testid="todo-item-toggle"]').click()
+
+    cy.get('.todo-list li').should('have.length', 1)
+    cy.get('.todo-list li').should('contain', 'Buy bread')
   })
 
   it('mark todo as incompleted', () => {
     cy.visit('https://todomvc.com/examples/react/dist/')
+
     cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
     cy.get('[data-testid="todo-item-toggle"]').click()
     cy.get('[data-testid="todo-item-toggle"]').click()
+
+    cy.get('.todo-list li').should('have.length', 1)
+    cy.get('.todo-list li').should('contain', 'Buy strawberries')
   })
 
   // it('edit todo', () => {
