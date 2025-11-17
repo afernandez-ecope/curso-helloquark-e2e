a20e4a644d5a033638f5bcc74daeaad50ec04e02
Alejandro Fernández <afernandez@ecope.es>
2025-11-17 18:47:22 +0100

    test: Refactor Cypress tests for TodoMVC to improve readability and add comments for clarity

--- cypress/e2e/todo_page.cy.js
+++ cypress/e2e/todo_page.cy.js
@@ -1,45 +1,48 @@
+// describe is used to group the tests
 describe('todomvc.com page test', () => {
 
-  it('add todo', () => {
+  // beforeEach is used to visit the page before each test
+  beforeEach(() => {
     cy.visit('https://todomvc.com/examples/react/dist/')
+  })
 
+  // it is used to test the add todo functionality
+  it('should add a todo', () => {
     cy.get('[data-testid="text-input"]').type('Buy milk').type('{enter}')
-
     cy.get('.todo-list li').should('have.length', 1)
     cy.get('.todo-list li').should('contain', 'Buy milk')
   })
 
-  it('mark todo as completed', () => {
-    cy.visit('https://todomvc.com/examples/react/dist/')
-
+  // it is used to test the mark todo as completed functionality
+  it('should mark a todo as completed', () => {
     cy.get('[data-testid="text-input"]').type('Buy bread').type('{enter}')
     cy.get('[data-testid="todo-item-toggle"]').click()
-
     cy.get('.todo-list li').should('have.length', 1)
     cy.get('.todo-list li').should('contain', 'Buy bread')
   })
 
-  it('mark todo as incompleted', () => {
-    cy.visit('https://todomvc.com/examples/react/dist/')
-
+  // it is used to test the mark todo as incompleted functionality
+  it('should mark a todo as incompleted', () => {
     cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
     cy.get('[data-testid="todo-item-toggle"]').click()
     cy.get('[data-testid="todo-item-toggle"]').click()
-
     cy.get('.todo-list li').should('have.length', 1)
   })
 
-  // it('edit todo', () => {
-  //   cy.visit('https://todomvc.com/examples/react/dist/')
-  //   cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
-  //   cy.get('[data-testid="todo-item-label"]').dblclick()
-  //   cy.get('[data-testid="input-container"]').type('Buy beer').type('{enter}')
-  // })
+  // it is used to test the edit todo functionality
+  it('should edit a todo', () => {
+    cy.get('[data-testid="text-input"]').type('Buy strawberries').type('{enter}')
+    cy.get('[data-testid="todo-item-label"]').should('be.visible').dblclick()
+    cy.focused().clear().type('Buy beer{enter}')
+    cy.get('.todo-list li').should('contain', 'Buy beer')
+  })
 
-  // it('delete todo', () => {
-  //   cy.visit('https://todomvc.com/examples/react/dist/')
-  //   cy.get('[data-testid="text-input"]').type('Buy oranges').type('{enter}')
-  //   cy.get('[data-testid="todo-item-label"]').trigger('mouseover')
-  // })
+  // it is used to test the delete todo functionality
+  it('should delete a todo', () => {
+    cy.get('[data-testid="text-input"]').type('Buy oranges').type('{enter}')
+    cy.contains('li', 'Buy oranges').trigger('mouseover')
+    cy.contains('li', 'Buy oranges').find('button.destroy').click({ force: true })
+    cy.contains('Buy oranges').should('not.exist')
+  })
 
 })
